ce966878f864a92bc5d4df9bf6c7a3552c422de0
PreethamNimmagadda02 <23je0653@iitism.ac.in>
2025-10-19 20:46:29 +0530

    Add manifest and theme color to index.html; register service worker in index.tsx

---
+++ sw.js
@@ -0,0 +1,74 @@
+const CACHE_NAME = 'college-central-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/logo.svg',
+  '/manifest.json'
+  // Critical JS/CSS will be cached on first visit by the fetch handler
+];
+
+self.addEventListener('install', event => {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('activate', event => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  // Don't cache Firestore requests, let the SDK handle offline persistence.
+  if (event.request.url.includes('firestore.googleapis.com')) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+  
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+
+        return fetch(event.request).then(
+          function(response) {
+            // Check if we received a valid response and it's a GET request
+            if(!response || response.status !== 200 || event.request.method !== 'GET') {
+              return response;
+            }
+
+            // IMPORTANT: Clone the response. A response is a stream
+            // and because we want the browser to consume the response
+            // as well as the cache consuming the response, we need
+            // to clone it so we have two streams.
+            var responseToCache = response.clone();
+
+            caches.open(CACHE_NAME)
+              .then(function(cache) {
+                cache.put(event.request, responseToCache);
+              });
+
+            return response;
+          }
+        );
+      })
+    );
+});
