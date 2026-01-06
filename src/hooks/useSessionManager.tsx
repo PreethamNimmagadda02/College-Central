fddd5623f96d594dc95bcde8d2926f3e735e3018
PreethamNimmagadda02 <23je0653@iitism.ac.in>
2026-01-06 18:55:48 +0530

    refactor: Remove unused React import from useSessionManager hook

--- src/hooks/useSessionManager.tsx
+++ src/hooks/useSessionManager.tsx
@@ -9,7 +9,7 @@
  * - Automatic logout with redirect
  */
 
-import React, { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { auth } from '@lib/firebase';
 
